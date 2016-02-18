a37404cae28d9e14b00c06b5234f7d5e4cf92bd3
adrianliaw <adrianliaw2000@gmail.com>
2016-02-18 17:20:30 +0800

    Add rooms functionality

--- im.js
+++ im.js
@@ -1,10 +1,42 @@
 Messages = new Mongo.Collection("messages");
+Rooms = new Mongo.Collection("rooms");
+
+
+FlowRouter.route("/", {
+  action() {
+    BlazeLayout.render("layout", {body: "RoomList"});
+  }
+});
+
+FlowRouter.route("/room/:roomId", {
+  action({roomId}) {
+    BlazeLayout.render("layout", {body: "Room"});
+  }
+})
 
 
 if (Meteor.isClient) {
-  Template.Board.helpers({
+
+  Template.RoomList.helpers({
+    rooms() {
+      return Rooms.find();
+    }
+  });
+
+  Template.RoomList.events({
+    "submit #new-room": function(event, template) {
+      event.preventDefault();
+      let roomId = Rooms.insert({name: template.find("#room-name").value});
+      FlowRouter.go(`/room/${roomId}`);
+    }
+  })
+
+  Template.Room.helpers({
+    room() {
+      return Rooms.findOne(FlowRouter.current().params.roomId);
+    },
     messages() {
-      return Messages.find();
+      return Messages.find({roomId: FlowRouter.current().params.roomId});
     },
     userDisplayName(userId) {
       let user = Meteor.users.findOne(userId);
@@ -12,13 +44,14 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.NewMessage.events({
+  Template.Room.events({
     "submit #new-message": function(event, template) {
       event.preventDefault();
       Messages.insert({
         text: template.find("#message-text").value,
         authorId: Meteor.userId(),
-        created: new Date()
+        created: new Date(),
+        roomId: FlowRouter.current().params.roomId
       });
       template.find("#message-text").value = "";
     } 
@@ -27,9 +60,10 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(() => {
-    if (Messages.findOne() === undefined) {
-      Messages.insert({text: "Hello world", created: new Date()});
-      Messages.insert({text: "Meteor rocks", created: new Date()});
+    if (Rooms.findOne() === undefined) {
+      let roomId = Rooms.insert({name: "Meteor Chats"});
+      Messages.insert({text: "Hello world", roomId: roomId, created: new Date()});
+      Messages.insert({text: "Meteor rocks", roomId: roomId, created: new Date()});
     }
   });
 }
